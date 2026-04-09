c614ef4c17db435327b50d783a0dc2770ecfcc13
altaevnikita <altaevnikita@gmail.com>
2026-04-09 20:01:21 +0600

    Add Sidebar and Time Calculator page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TimeCalculator } from './pages/time-calculator/time-calculator';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TimeCalculator }
+];
---
+++ src/app/pages/time-calculator/time-calculator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-calculator',
+  templateUrl: './time-calculator.html',
+  styleUrl: './time-calculator.scss'
+})
+export class TimeCalculator {}
